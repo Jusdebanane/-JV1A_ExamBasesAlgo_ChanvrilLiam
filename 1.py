6d61f1f35cd5e19b199f4faddb91a7fa06c739c9
LIAM <lc.liamchanvril@gmail.com>
2022-11-22 13:44:09 +0100

    commit

--- 1.py
+++ 1.py
@@ -1,8 +1,37 @@
+#1
 MyTable = [1,8,21,22,95,56,42]
 save = 0
-pos = 0
 
 save = MyTable[0]
 MyTable[0]= MyTable[3]
 MyTable[3] = save
+print(MyTable)
 input()
+
+#2
+MyTable = [1,8,21,22,95,56,42]
+save = 0
+
+for i in range(1,len(MyTable)):
+    if (MyTable[i]<MyTable[i-1]):
+        save = MyTable[i]
+        MyTable[i]=MyTable[i-1]
+        MyTable[i-1]=save
+print(MyTable)
+input()
+
+#3
+MyTable = [1,8,21,22,95,56,42]
+save = 0
+
+for j in range(1,len(MyTable)):
+    for i in range(1,len(MyTable)):
+        if (MyTable[i]<MyTable[i-1]):
+            save = MyTable[i]
+            MyTable[i]=MyTable[i-1]
+            MyTable[i-1]=save
+print(MyTable)
+input()
+
+#4
+#A chaque boucle "j" on parcours a nouveau tout le tableau et meme s'il est déjà trié
